86905d1c7e1d088026bbe43c387c9103827cf766
Steven De Coeyer <git@stevendecoeyer.be>
2025-08-01 13:50:47 +0200

    Adds tofu-ls to LSP

--- init.lua
+++ init.lua
@@ -642,6 +642,7 @@ require('lazy').setup({
       vim.list_extend(ensure_installed, {
         'rubocop',
         'ruby-lsp',
+        'tofu-ls',
         'stylua', -- Used to format Lua code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
